c38b5927464e82b91bdfeeaa81db5b7a2fa36223
Ari Weinstein <arixmail@gmail.com>
2018-11-17 11:59:13 -0800

    Initial Commit

---
+++ PrevuePackage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PrevuePackage
+//
+//  Created by Ari on 11/17/18.
+//  Copyright © 2018 Vertex. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
